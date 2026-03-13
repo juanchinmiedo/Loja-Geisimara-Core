2846f3a5e89374bc2d3713e657ad730f06ac5a84
JuanSaladoKaphammel <jkaphammel@gmail.com>
2026-03-13 15:29:31 +0000

    Chore - add Firebase local options for CI

---
+++ lib/firebase_options_local.dart
@@ -0,0 +1,31 @@
+class FirebaseLocal {
+  static const messagingSenderId = '585284151370';
+  static const projectId = 'agenda-loja';
+  static const storageBucket = 'agenda-loja.firebasestorage.app';
+
+  // Web
+  static const webApiKey = 'AIzaSyD37NgdFh9EjSax4r1H4UrM5S6GmQ_Xg4A';
+  static const webAppId = '1:585284151370:web:6304404404c3e1006b9eb3';
+  static const authDomain = 'agenda-loja.firebaseapp.com';
+  static const measurementId = 'G-NP1D1KDKW7';
+
+  // Android
+  static const androidApiKey = 'AIzaSyDycv6JmfaXDztDg2sOvLJ36Oguk6UdsIg';
+  static const androidAppId = '1:585284151370:android:0624f549874c8f1c6b9eb3';
+
+  // iOS
+  static const iosApiKey = 'AIzaSyAFZpXJb9X7NZ3AIkf8zEnSUSnuvxTmECA';
+  static const iosAppId = '1:585284151370:ios:5e83e4af749ba41a6b9eb3';
+  static const iosBundleId = 'com.example.salonApp';
+
+  // macOS
+  static const macosApiKey = 'AIzaSyAFZpXJb9X7NZ3AIkf8zEnSUSnuvxTmECA';
+  static const macosAppId = '1:585284151370:ios:5e83e4af749ba41a6b9eb3';
+  static const macosBundleId = 'com.example.salonApp';
+
+  // Windows / Linux (si aplica)
+  static const windowsApiKey = 'AIzaSyD37NgdFh9EjSax4r1H4UrM5S6GmQ_Xg4A';
+  static const windowsAppId = '1:585284151370:web:eaf3e75095a2d1916b9eb3';
+  static const windowsAuthDomain =  'agenda-loja.firebaseapp.com';
+  static const windowsMeasurementId = 'G-XMB13X76KF';
+}
